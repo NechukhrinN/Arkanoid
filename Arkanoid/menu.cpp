43fd91d24b361255911ed1465b0ab903407dd86f
Nikita Nechukhrin <nikita.nechukhrin@gmail.com>
2018-11-24 22:53:23 +0300

    Little fixes

--- Arkanoid/menu.cpp
+++ Arkanoid/menu.cpp
@@ -10,22 +10,33 @@ void menu(sf::RenderWindow & window) {
 	sf::Sprite menu1(menuTexture1), menu2(menuTexture2), menu3(menuTexture3), about(aboutTexture), menuBg(menuBackground);
 	bool isMenu = 1;
 	int menuNum = 0;
+	window.draw(menuBg);
 	menu1.setPosition(100, 30);
 	menu2.setPosition(100, 90);
 	menu3.setPosition(100, 150);
-	menuBg.setPosition(345, 0);
+	//menuBg.setPosition(345, 0);
 
 	while (isMenu)
 	{
+		sf::Event e;
+		while (window.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed)
+				window.close();
+		}
+
 		menu1.setColor(sf::Color::White);
 		menu2.setColor(sf::Color::White);
 		menu3.setColor(sf::Color::White);
 		menuNum = 0;
 		window.clear(sf::Color(129, 181, 221));
 
-		if (sf::IntRect(100, 30, 300, 50).contains(sf::Mouse::getPosition(window))) { menu1.setColor(sf::Color::Blue); menuNum = 1; }
-		if (sf::IntRect(100, 90, 300, 50).contains(sf::Mouse::getPosition(window))) { menu2.setColor(sf::Color::Blue); menuNum = 2; }
-		if (sf::IntRect(100, 150, 300, 50).contains(sf::Mouse::getPosition(window))) { menu3.setColor(sf::Color::Blue); menuNum = 3; }
+		if (sf::IntRect(100, 30, 300, 50).contains(sf::Mouse::getPosition(window))) { menu1.setColor(sf::Color::Blue); 
+		menuNum = 1; }
+		if (sf::IntRect(100, 90, 300, 50).contains(sf::Mouse::getPosition(window))) { menu2.setColor(sf::Color::Blue); 
+		menuNum = 2; }
+		if (sf::IntRect(100, 150, 300, 50).contains(sf::Mouse::getPosition(window))) { menu3.setColor(sf::Color::Blue);
+		menuNum = 3; }
 
 		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
 		{
